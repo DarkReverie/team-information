05b987ce02720c3210fa62a4c7a4cf08dda2dd3f
DarkReverie <danylo.s2009@gmail.com>
2023-12-05 17:32:05 +0200

    feat/ new template format

---
+++ my-team-dream/src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import TeamMemberItem from '@/components/TeamMemberItem.vue';
+import type { TeamMember } from '@/shared/types';
+
+const teamMembers: TeamMember[] = [
+  {
+    id: 1,
+    name: 'Viktor Baran',
+    position: 'Project Manager',
+    image: 'https://avatars.githubusercontent.com/u/125183903?v=4',
+  }
+];
+</script>
+
+<template>
+  <main class="root">
+    <div class="members">
+      <div v-for='teamMember in teamMembers' :key='teamMember.id'>
+        <TeamMemberItem :teamMember=teamMember />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.root {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.members {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 60rem;
+  gap: 1rem;
+}
+</style>
---
+++ my-team-dream/src/components/TeamMemberItem.vue
@@ -0,0 +1,52 @@
+<script setup lang='ts'>
+import type { TeamMember } from '@/shared/types';
+
+defineProps<{
+  teamMember: TeamMember;
+}>()
+</script>
+
+<template>
+  <main>
+    <h1>{{ teamMember.name }}</h1>
+    <p>{{ teamMember.position }}</p>
+    <img :src='teamMember.image' :alt='teamMember.name' />
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: var(--color-background-mute);
+  border-radius: 1rem;
+  padding: 1.5rem;
+  width: 18rem;
+  transition: scale .3s ease-in-out;
+  text-align: center;
+}
+
+main:hover {
+  background-color: var(--color-background-soft);
+  scale: 1.05;
+}
+
+
+h1 {
+  font-size: 1.5rem;
+  margin: 0;
+  color: var(--color-heading);
+}
+
+p {
+  font-size: 1rem;
+  margin: 0;
+  color: var(--color-text);
+}
+
+img {
+  border-radius: 50%;
+  width: 12rem;
+  aspect-ratio: 1 / 1;
+  padding: 1rem;
+}
+
+</style>
