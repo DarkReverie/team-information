1c0acffad10ad4f162e0be6a2e4b70b90126dbbc
annnychkka <aniutka.lviv@gmail.com>
2023-12-05 18:57:26 +0200

    feat/add-anna-suslova-info

--- my-team-dream/src/App.vue
+++ my-team-dream/src/App.vue
@@ -8,6 +8,12 @@ const teamMembers: TeamMember[] = [
     name: 'Viktor Baran',
     position: 'Project Manager',
     image: 'https://avatars.githubusercontent.com/u/125183903?v=4',
+  },
+  {
+    id: 2,
+    name: 'Suslova Anna',
+    position: 'QA Engineer',
+    image: 'https://avatars.githubusercontent.com/u/51747613?v=4',
   }
 ];
 </script>
