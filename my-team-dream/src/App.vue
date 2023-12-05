08e781447d99a7d7d0c5bfe0b4c1f2dd6f16f5ae
DarkReverie <danylo.s2009@gmail.com>
2023-12-05 18:28:44 +0200

    feat/added-danylo-savytskyi-info

--- my-team-dream/src/App.vue
+++ my-team-dream/src/App.vue
@@ -8,6 +8,12 @@ const teamMembers: TeamMember[] = [
     name: 'Viktor Baran',
     position: 'Project Manager',
     image: 'https://avatars.githubusercontent.com/u/125183903?v=4',
+  },
+  {
+    id: 2,
+    name: 'Danylo Savytskyi',
+    position: 'Project Manager',
+    image: 'https://avatars.githubusercontent.com/u/107762418?s=400&u=b1347f293f12a1399ada2731cad8610890be2375&v=4',
   }
 ];
 </script>
